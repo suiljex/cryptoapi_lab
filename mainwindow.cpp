8d54a7e12bfcab37f4e3ba687b8235e127927b73
Гарнец Игорь Дмитриевич <garik.pro100@gmail.com>
2018-03-18 11:50:36 +0700

    ADD Первая версия проекта

---
+++ mainwindow.cpp
@@ -0,0 +1,209 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+  DWORD dwIndex=0;
+  DWORD dwType;
+  DWORD cbName;
+  LPTSTR pszName;
+
+  while (CryptEnumProviders(dwIndex, NULL, 0, &dwType, NULL, &cbName))
+  {
+    if (!cbName)
+      break;
+    if (!(pszName = (LPTSTR)LocalAlloc(LMEM_ZEROINIT, cbName)))
+      return;
+    if (!CryptEnumProviders(dwIndex++, NULL, 0, &dwType, pszName, &cbName))
+    {
+//      Error("CryptEnumProviders");
+      return;
+    }
+    ui->listWidget->addItem(QString("--------------------------------"));
+    ui->listWidget->addItem(QString("Provider name: ") + QString::fromWCharArray(pszName));
+    ui->listWidget->addItem(QString("Provider type: ") + QString::number(dwType));
+    LocalFree(pszName);
+  }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+  HCRYPTPROV hProv;
+  HCRYPTKEY hKey;
+  HCRYPTHASH hHash;
+
+  // Получение контекста криптопровайдера
+  if (!CryptAcquireContext(&hProv, NULL, NULL,PROV_RSA_FULL, NULL))
+  {
+//    Error("CryptAcquireContext");
+    return;
+  }
+  ui->listWidget_2->addItem("Cryptographic provider initialized");
+
+  // Cоздание хеш-объекта
+  if(!CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash))
+  {
+//    Error("CryptCreateHash");
+    return;
+  }
+  ui->listWidget_2->addItem("Hash created");
+
+  // Передача хешируемых данных хэш-объекту.
+
+  std::string pass = ui->lineEdit->text().toStdString().c_str();
+  DWORD count = pass.length();
+
+  if(!CryptHashData(hHash, (BYTE*)(pass.c_str()), count, 0))
+  {
+//    Error("CryptHashData");
+    return;
+  }
+  ui->listWidget_2->addItem("Hash data loaded");
+
+  if(!CryptDeriveKey(hProv, CALG_RC4, hHash, NULL, &hKey))
+  {
+    return;
+  }
+  ui->listWidget_2->addItem("Key Derived");
+
+  std::wstring mess = ui->textEdit->toPlainText().toStdWString();
+  count = mess.length();
+
+  if(!CryptEncrypt(hKey, 0, true, 0, (BYTE*)mess.c_str(), &count, mess.length()))
+  {
+    return;
+  }
+
+  ui->listWidget_2->addItem("Text Encrypted");
+  ui->textEdit_2->setText(QString::fromStdWString(mess));
+
+  std::wofstream fout;
+  fout.open("output.txt");
+  if(fout.is_open())
+  {
+    fout << mess;
+    ui->listWidget_2->addItem("File");
+    fout.close();
+  }
+
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+  HCRYPTHASH hHash;
+
+  // Получение контекста криптопровайдера
+  if (!CryptAcquireContext(&hProv_asym, ui->lineEdit_2->text().toStdWString().c_str(), NULL,PROV_RSA_FULL, CRYPT_NEWKEYSET))
+  {
+//    Error("CryptAcquireContext");
+    return;
+  }
+
+  ui->listWidget_2->addItem("Cryptographic provider initialized");
+
+  // Генерация ключа для тестирования
+  if (!CryptGenKey(hProv_asym, AT_KEYEXCHANGE, CRYPT_EXPORTABLE | CRYPT_ENCRYPT | CRYPT_DECRYPT, &hKey_asym))
+  {
+    return;
+  }
+  ui->listWidget_2->addItem("Session key generated");
+
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+  HCRYPTKEY hPublicKey;
+
+  // Получение ключа для экспорта ключа шифрования
+  if (!CryptGetUserKey(hProv_asym, AT_KEYEXCHANGE, &hPublicKey))
+  {
+    return;
+  }
+
+  ui->listWidget_2->addItem("Public key is received");
+
+  DWORD count = 0;
+
+  // Получение размера массива, используемого для экспорта ключа
+  if (!CryptExportKey(hPublicKey, 0, PUBLICKEYBLOB, 0, NULL, &count))
+  {
+    return;
+  }
+
+  // Инициализация массива, используемого для экспорта ключа
+  BYTE* data = static_cast<BYTE*>(malloc(count));
+  ZeroMemory(data, count);
+
+  // Экспорт ключа шифрования
+  if (!CryptExportKey(hPublicKey, 0, PUBLICKEYBLOB, 0, data, &count))
+  {
+    return;
+  }
+
+  ui->listWidget_2->addItem("Key's export completed");
+
+  std::ofstream fout;
+  fout.open("output.txt");
+  if(fout.is_open())
+  {
+    fout.write((char*)data, count);
+    ui->listWidget_2->addItem("Key exported");
+    fout.close();
+  }
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+  HCRYPTKEY hNewKey;
+  BYTE* data;
+  DWORD count;
+
+  std::ifstream fin;
+  fin.open("output.txt", std::ios_base::binary);
+  if(fin.is_open())
+  {
+    fin.seekg(0, std::ios_base::end);
+    count = fin.tellg();
+
+    data = static_cast<BYTE*>(malloc(count));
+    ZeroMemory(data, count);
+
+    fin.seekg(0, std::ios_base::beg);
+    for (int br = 0; br < count; ++br)
+    {
+      fin.read((char*)data+br, 1);
+    }
+    ui->listWidget_2->addItem("Key read");
+    fin.close();
+
+    if (!CryptAcquireContext(&hProv_new, NULL, NULL,PROV_RSA_FULL, NULL))
+    {
+  //    Error("CryptAcquireContext");
+      return;
+    }
+
+    if(!CryptImportKey(hProv_new, data, count, 0, 0, &hNewKey))
+    {
+      DWORD error = GetLastError();
+      return;
+    }
+    ui->listWidget_2->addItem("Key's import completed");
+  }
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,41 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    HCRYPTPROV hProv_asym;
+    HCRYPTKEY hKey_asym;
+    HCRYPTPROV hProv_new;
+};
+
+#endif // MAINWINDOW_H
